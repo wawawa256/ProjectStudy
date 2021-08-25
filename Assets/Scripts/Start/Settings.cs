6ceca735f16ef0f083674ca752002477a7d60461
shogo <81398402+shogo-0501@users.noreply.github.com>
2021-08-26 00:30:39 +0900

    Quetion
    できるようになたよ

--- Assets/Scripts/Start/Settings.cs
+++ Assets/Scripts/Start/Settings.cs
@@ -17,8 +17,12 @@ public class Settings : MonoBehaviour
         
     }
 
-    public void OnClickWeb()
+    public void OnClickWiki()
     {
         Application.OpenURL("http://ucchon.php.xdomain.jp/wiki.php");//""の中には開きたいWebページのURLを入力します
+    }
+    public void OnClickQuetion()
+    {
+        Application.OpenURL("http://ucchon.php.xdomain.jp/newindex.php");//""の中には開きたいWebページのURLを入力します
     }
 }
