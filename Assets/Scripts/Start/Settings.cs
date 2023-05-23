6f33c09c091b5d531c954af5fc1d5dd04e19a858
takumilktea1330 <81323735+takumilktea1330@users.noreply.github.com>
2023-05-23 15:38:00 +0900

    prepare

--- Assets/Scripts/Start/Settings.cs
+++ Assets/Scripts/Start/Settings.cs
@@ -5,18 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class Settings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void OnClickWiki()
     {
         Application.OpenURL("http://ucchon.php.xdomain.jp/wiki.php");//""の中には開きたいWebページのURLを入力します
